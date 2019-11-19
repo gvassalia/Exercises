29c9690b187040dbaae88278b6347811fae3f0df
ogvassalia <otar.gvassalia@fleetmind.com>
2019-11-19 11:33:39 -0500

    Polindrome added

---
+++ src/main/polindrome/Polindrome.java
@@ -0,0 +1,49 @@
+package polindrome;
+
+public class Polindrome {
+	private String text;
+	private boolean isValid;
+	
+	public Polindrome() {
+	}
+	
+	public Polindrome(String text) {
+		setText(text);
+		isValid = check();
+	}
+	
+	public void setText(String arg) {
+		text = arg;
+		
+		if (text == null) {
+			return;
+		}
+		
+		text = text.trim();
+	}
+	
+	public String getText() {
+		return text;
+	}
+	
+	public boolean isValid() {
+		return isValid;
+	}
+	
+	private boolean check() {
+		if (text == null || text.length() == 0) {
+			
+		}
+		for (int i = 0, j = text.length() - 1; i != j; i++, j--) {
+			if (i > j) {
+				break;
+			}
+			
+			if (text.charAt(i) != text.charAt(j)) {
+				return false;
+			}
+		}
+		
+		return true;
+	}
+}
